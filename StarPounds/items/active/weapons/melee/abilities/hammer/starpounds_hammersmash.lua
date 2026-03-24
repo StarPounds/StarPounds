e33de6b0bac4b5f22d951ec22a9593305da32de3
Apple <141291111+LittleVulpine@users.noreply.github.com>
2026-03-25 05:46:39 +1000

    Hammer smash corrects for weight

--- StarPounds/items/active/weapons/melee/abilities/hammer/starpounds_hammersmash.lua
+++ StarPounds/items/active/weapons/melee/abilities/hammer/starpounds_hammersmash.lua
@@ -1,12 +1,15 @@
 local HammerSmashFire_old = HammerSmash.fire
 function HammerSmash:fire(...)
   local defaultDamage = self.damageConfig.baseDamage
+  local defaultMomentum = self.smashMomentum
 
   local starPounds = getmetatable ''.starPounds
   if starPounds then
     self.damageConfig.baseDamage = defaultDamage * (1 + starPounds.getStat("smashDamage") * starPounds.moduleFunc("size", "effectScaling"))
+    self.smashMomentum = vec2.mul(defaultMomentum, starPounds.weightMultiplier)
   end
 
   HammerSmashFire_old(self, ...)
   self.damageConfig.baseDamage = defaultDamage
+  self.smashMomentum = defaultMomentum
 end
