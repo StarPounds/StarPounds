9dfda55f1c49ff7bd99efa6ad41397bd2f9cac41
Apple <141291111+LittleVulpine@users.noreply.github.com>
2026-03-31 06:52:58 +1000

    Whoops, variance was only positive

--- StarPounds/scripts/projectiles/starpoundscaloricneedlerthorn.lua
+++ StarPounds/scripts/projectiles/starpoundscaloricneedlerthorn.lua
@@ -2,7 +2,7 @@ require "/scripts/util.lua"
 
 function init()
   self.timeToLive = config.getParameter("timeToLive")
-  self.timeToLiveVariance = math.random() * config.getParameter("timeToLiveVariance", 0)
+  self.timeToLiveVariance = (math.random() * 2 - 1) * config.getParameter("timeToLiveVariance", 0)
 
   projectile.setTimeToLive(self.timeToLive + self.timeToLiveVariance)
 end
