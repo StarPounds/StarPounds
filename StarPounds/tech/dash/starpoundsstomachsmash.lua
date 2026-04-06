538932893dc27dce9d7102a175c81d819923b4e5
Apple <141291111+LittleVulpine@users.noreply.github.com>
2026-04-06 21:27:34 +1000

    Dashes stop correctly when fat

--- StarPounds/tech/dash/starpoundsstomachsmash.lua
+++ StarPounds/tech/dash/starpoundsstomachsmash.lua
@@ -32,10 +32,26 @@ end
 
 local endDash_old = endDash or function() end
 function endDash(...)
+  -- Stop harder for weight.
+  if self.stopAfterDash then
+    self.stopAfterDash = false
+
+    starPounds = getmetatable ''.starPounds or {}
+    local movementParams = mcontroller.baseParameters()
+    local runSpeed = movementParams.runSpeed * (starPounds.movementMultiplier or 1)
+    local currentVelocity = mcontroller.velocity()
+    if math.abs(currentVelocity[1]) > runSpeed then
+      mcontroller.setVelocity({runSpeed * self.dashDirection, 0})
+    end
+    mcontroller.controlApproachXVelocity(self.dashDirection * runSpeed, self.dashControlForce)
+  end
+
   self.dashControlForce = self.baseDashControlForce
   self.dashSpeed = self.baseDashSpeed
 
   endDash_old(...)
+
+  self.stopAfterDash = true
 end
 
 
