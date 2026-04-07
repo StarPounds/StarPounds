7adc3d51170719f493694115e8310f507614dc0a
Apple <141291111+LittleVulpine@users.noreply.github.com>
2026-04-08 04:22:48 +1000

    Bake extended races into the main mod

--- StarPounds/objects/fattiesbar/fattiesfurnishings/fattiesfurnishings.lua
+++ StarPounds/objects/fattiesbar/fattiesfurnishings/fattiesfurnishings.lua
@@ -2,9 +2,13 @@ function init()
   self.buyFactor = config.getParameter("buyFactor", root.assetJson("/merchant.config").defaultBuyFactor)
 
   object.setInteractive(true)
+
+  self.species = {generic = true, ancient = true}
 end
 
-function onInteraction(args)
+function buildInteraction()
+  if self.interactData then return self.interactData end
+
   local interactData = config.getParameter("interactData")
   interactData.recipes = {}
 
@@ -12,9 +16,9 @@ function onInteraction(args)
   local tabs = interactData["modTab"]
   local posY = 277
   local categories = {
-      type = "radioGroup",
-      toggleMode = false,
-      buttons = {}
+    type = "radioGroup",
+    toggleMode = false,
+    buttons = {}
   }
 
   if #tabs > 0 then
@@ -66,12 +70,56 @@ function onInteraction(args)
 
   for genre,objects in pairs(storeInventory) do addRecipes(interactData, objects, genre) end
 
-  return { "OpenCraftingInterface", interactData }
+  self.interactData = interactData
+  return interactData
+end
+
+
+function onInteraction(args)
+  return { "OpenCraftingInterface", buildInteraction() }
 end
 
 function addRecipes(interactData, items, category)
   for i, item in ipairs(items) do
-    interactData.recipes[#interactData.recipes + 1] = generateRecipe(item, category)
+    local showItem = false
+
+    local checkSpecies = {}
+    local conf
+    if type(item) == "string" then
+      conf = root.itemConfig(item).config
+    elseif type(item) == "table" then
+      conf = root.itemConfig(item.output).config
+    end
+
+    if conf.race then
+      checkSpecies[#checkSpecies + 1] = conf.race
+    end
+
+    if conf.races and type(conf.races) == "table" then
+      for _, species in ipairs(conf.races) do
+        checkSpecies[#checkSpecies + 1] = species
+      end
+    end
+
+    if #checkSpecies == 0 then
+      showItem = true
+    end
+
+    for _, species in ipairs(checkSpecies) do
+      -- Cache checked species.
+      if species and (self.species[species] == nil) then
+        self.species[species] = pcall(function () root.npcVariant(species, "base", 1, 1) return true end)
+        if not self.species[species] then
+          sb.logInfo(species.." is invalid")
+        end
+      end
+
+      showItem = showItem or self.species[species]
+    end
+
+    if showItem then
+      interactData.recipes[#interactData.recipes + 1] = generateRecipe(item, category)
+    end
   end
 end
 
